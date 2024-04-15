e52999ccbeef950544dbf961e6ae90d496195e77
mhhan <mun01180@gmail.com>
2024-04-15 09:10:15 +0900

    최종 모듈화 마무리

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,27 +1,10 @@
 import { createStore } from "vuex";
-import * as getters from './getters';
-import * as mutations from './mutations'
-
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-    return arr;
-  },
-};
+import todoApp from './modules/todoApp'
 
 const store = createStore({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters,
-  mutations
+  modules: {
+    todoApp
+  }
 });
 
 export { store };
